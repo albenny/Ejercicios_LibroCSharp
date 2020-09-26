8e0a7220b219bdbb225be3166ec075b2d3de32c5
albenny <70269274+albenny@users.noreply.github.com>
2020-09-26 14:30:22 -0400

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,13 +1,25 @@
 ﻿using System;
+using System.Linq;
 
-namespace Capitulos
+namespace Cap7
 {
-    class Program
+    class Ejercicio1
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("albenny !");
-            Console.WriteLine("estudia ingenieria en sistemas y computos")
-        }
+{
+    int[] nota = new int[15];
+    int N, max = nota[0], min = nota[0];
+    for(int i = 0; i < 15; i++)
+    {
+        Console.Write("ingresos de la nota: ");
+        N = int.Parse(Console.ReadLine());
+        nota[i] = N;
+    }
+
+    Console.Write("\n\nLa nota maxima es: " + nota.Max());
+    Console.Write("\n\nLa nota minima es: " + nota.Min());
+    Console.ReadKey();
+}
     }
 }
+
